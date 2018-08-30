8a9c1a43ff70451e8fe92f281b4529b9f5f61acc
Daniel <daniel.ahnuc10@gmail.com>
2018-08-30 20:14:16 -0300

    Implementação do IQI

--- lab2/raiz.c
+++ lab2/raiz.c
@@ -40,7 +40,34 @@ int bissecao (double a, double b, int p, double (*f) (double x), double* r)
 
 int IQI (double x0, double x1, double x2, int p, double (*f) (double x), double* r)
 {
+  double erro, fx0, fx1, fx2, detAc, detA;
+  int interacoes = 0;
   
+  erroEsp = 0.5*pow(10,-(double)p);
+  
+  while(abs(f(x2)) > erroEsp)
+  {
+    fx0 = f(x0);
+    fx1 = f(x1);
+    fx2 = f(x2);
+    
+    fx02 = (fx0*fx0);
+    fx12 = (fx1*fx1);
+    fx22 = (fx2*fx2);
+    
+    detA = fx02 * fx1 * 1 + fx0 * 1 * fx22 + fx12 * fx2 * 1 - (fx22 * fx1 * 1 + fx2 * 1 *  fx02 + fx0 * fx12 * 1);
+    
+    detAc = fx02 * fx1 * x2 + fx0 * x1 * fx22 + fx12 * fx2 * x0 - (fx22 * fx1 * x0 + fx2 * x1 *  fx02 + fx0 * fx12 * x2);
+    
+    c = detAc/detA;
+    x0 = x1;
+    x1 = x2;
+    x2 = c;
+    interacoes++;
+  }
+  *r = c;
+  
+  return interacoes;
 }
 
 
