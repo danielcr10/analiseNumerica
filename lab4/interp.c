9a3749034f96751b9d761d467d29c10151515286
DANIEL CUNHA RIOS <c1512920@pipa.grad.inf.puc-rio.br>
2018-09-12 14:56:22 -0300

    funcoes lab4

---
+++ lab4/interp.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#define pi=3.14159265359 
+
+void chebyshev (int n, double a, double b, double* xi) //xi vetor pre-alocado
+{
+  //xis - valores distribuidos no intervalo
+  xi = ((b - a)/2) * cos((beta * pi)/2*n) + (a + b)/2;
+}
+
+void newtonCoef (int n, double* xi, double (*f) (double), double* bi)
+{
+
+}
+
+double newtonAval (int n, double* xi, double* bi, double x)
+{
+
+}
+
+//2a ja ta feito
+//comparar a minha fnc com a fnc cos da matematica
