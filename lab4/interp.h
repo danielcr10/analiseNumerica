074e319ab5621e7def06a14e4fc5e7965fb7feba
Daniel <daniel.ahnuc10@gmail.com>
2018-09-13 23:40:30 -0300

    Arquivos para entrega

--- lab4/interp.h
+++ lab4/interp.h
@@ -1,3 +1,8 @@
+#define pi 3.14159265359 
+typedef double ** Matriz;
+static double calculaF (int i, int j, double * xi, double (*f) (double));
+static Matriz mat_cria (int m, int n);
+
 void chebyshev (int n, double a, double b, double* xi);
 
 void newtonCoef (int n, double* xi, double (*f) (double), double* bi);
--- lab4/main.c
+++ lab4/main.c
@@ -1,7 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "interp.h"
 
+double f(double x)
+{
+	return cos(x);
+}
 
 int main()
 {
-  
-  return 0;
+	int n = 6;
+	double aval;
+	double *xi = (double *) malloc(n * sizeof(double));
+	double *bi = (double *) malloc(n * sizeof(double));
+
+	if(xi == NULL) {
+		printf("Falta memoria. \n");
+		exit(1);
+	}
+	if(bi == NULL) {
+		printf("Falta memoria. \n");
+		exit(1);
+	}
+
+	chebyshev (n, 0, pi/2, xi);
+
+	printf("\n\n");
+	newtonCoef (n, xi, &f, bi);
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%f\n", bi[i]);
+	}
+	
+	aval = newtonAval (n, xi, bi, pi/4);
+	printf("\n\n%.15f\n", aval);
+	printf("%.15f\n", cos(M_PI/4));
+
+	return 0;
 }
