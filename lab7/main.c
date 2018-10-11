637457169745c2d7814fd16d2fbf163c829e1e86
DANIEL CUNHA RIOS <c1512920@sono.grad.inf.puc-rio.br>
2018-10-11 16:23:49 -0300

    Inicio da main

---
+++ lab7/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(x){
+  return cos(x) - 2*sin(x);
+}
+
+double fLinha(x){
+  return -sin(x) - 2*cos(x);
+}
+
+int main(){
+  //Avalia h ótimo em f(x) e em fLinha(x) e compara
+  
+  
+  return 0;
+}
