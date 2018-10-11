eddc9a1ef34be9b8930604ad2abfbdd812acd635
DANIEL CUNHA RIOS <c1512920@sono.grad.inf.puc-rio.br>
2018-10-11 16:13:39 -0300

    Finalização lab7 ex 1

--- lab7/integral.c
+++ lab7/integral.c
@@ -23,11 +23,32 @@ double h_otimo (double (*f) (double x), double (*fl) (double x), double x){
 }
 
 double simpson (double (*f) (double), double a, double b, int n){
-  //valor pra simpson
+  double hPassos, resul = 0, h, m;
+  hPassos = (b - a)/n;
+  
+  for(int i = 0; i < n; i++)
+  {
+    b = a + hPassos;
+    m = (a + b)/2;
+    h = b - a;
+    resul += h/6 * ( f(a) + 4*f(m) + f(b) );
+    a = b;
+  }
+  return resul;  
 }
 
 double pontomedio (double (*f) (double), double a, double b, int n){
-
+  double hPassos, m, h, resul = 0;
+  hPassos = (b - a)/n;
+  
+  for(int i = 0; i < n; i++){
+    b = a + hPassos;
+    m = (a + b)/2;
+    h = b - a;
+    resul += h * f(m);
+    a = b;
+  }
+  return resul;
 }
 
 int main(){
