21fb1b3138b90b47270b04624676f48c114063a5
DANIEL CUNHA RIOS <c1512920@lopes-mendes.grad.inf.puc-rio.br>
2018-10-17 14:34:21 -0300

    Prototipo das funções

---
+++ lab8/simpson.c
@@ -0,0 +1,15 @@
+double DoubleSimpson (double a, double b, double (*f) (double x), double* v){
+
+}
+
+double AdaptiveSimpson (double a, double b, double (*f) (double x), double tol){
+//Valores encontrados 0.162278 0.682689 09999779 de 0 a 1
+}
+
+double Quadratura2 (double a, double b, double (*f) (double x)){
+
+}
+
+double Quadratura3 (double a, double b, double (*f) (double x)){
+
+}
---
+++ lab8/simpson.h
@@ -0,0 +1,7 @@
+double DoubleSimpson (double a, double b, double (*f) (double x), double* v);
+
+double AdaptiveSimpson (double a, double b, double (*f) (double x), double tol);
+
+double Quadratura2 (double a, double b, double (*f) (double x));
+
+double Quadratura3 (double a, double b, double (*f) (double x));
