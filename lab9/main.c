c90ce1073229f4263ff67f67d0c0c208d058936b
DANIEL CUNHA RIOS <c1512920@catuama.grad.inf.puc-rio.br>
2018-10-24 15:56:19 -0200

    Update

---
+++ lab9/main.c
@@ -0,0 +1,11 @@
+/*
+  RESP
+  Euler: (erro esperado - valor espera/erro esperado)
+  h=0.01 -> erro relativo = 0.039
+  h=0.001 -> erro relativo = 0.004
+  h=0.0001 -> erro relativo = 0.0004
+  
+  Euler Adaptativo:
+  erro relativo = 0.0002
+  Renomear para edo
+*/
--- lab9/ode.c
+++ lab9/ode.c
@@ -1,5 +1,21 @@
 double Euler (double t0, double t1, double h, double y0, double (*f) (double t, double y)){
-
+  //t0 - tempo inicial
+  //t1 - tempo final
+  //h - passo de integração
+  //y(t0) - valor inicial
+  //f(t,y(t)) - função derivada
+  //y(t1) - retorno do valor no tempo final  
+  
+  double taux, yaux;
+  
+  taux = t0;
+  yaux = y0;
+  for(i = 1, n){
+    y = y + h*f(t,y)
+    t=t+h
+  }
+  return y;
+    
 }
 
 double EulerAdapt (double t0, double t1, double h0, double y0, double (*f) (double t, double y), double tol){
