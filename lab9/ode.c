326c2ee95b6bf06c41de6fd16bf29b74494a1aaa
DANIEL CUNHA RIOS <c1512920@catuama.grad.inf.puc-rio.br>
2018-10-24 14:27:12 -0200

    Inicio lab9

---
+++ lab9/ode.c
@@ -0,0 +1,7 @@
+double Euler (double t0, double t1, double h, double y0, double (*f) (double t, double y)){
+
+}
+
+double EulerAdapt (double t0, double t1, double h0, double y0, double (*f) (double t, double y), double tol){
+
+}
