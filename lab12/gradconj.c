a740b59a747c08e0a8d2754994af2ae385d3ebfb
DANIEL CUNHA RIOS <c1512920@trindade.grad.inf.puc-rio.br>
2018-11-14 14:50:27 -0200

    lab12

---
+++ lab12/gradconj.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+//Quando a norma-2 do residuo for menor que a tolerancia especificada, a solucao é considerada válida e as iteracoes devem ser interrompidas
+
+//Sua funcao deve atualizar o valor de x com a solucao do sistema e retornar o numero de iteracoes realizadas.
+
+int GradConj (int n, double** A, double* b, double* x, double tol){
+  
+}
+
+int GradConjJacobi (int n, double** A, double* b, double* x, double tol){
+
+}
